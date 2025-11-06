f77aa369e3073d51119e8ad8dac633b1762ee3d7
Frankz69 <frankz69@seas.upenn.edu>
2025-11-06 09:53:23 -0500

    Starter code files imported (Frank)

--- src/SpellCheckerRunner.java
+++ src/SpellCheckerRunner.java
@@ -1,7 +1,7 @@
-// Do not modify this file whatsoever.
+// DO NOT MODIFY THIS FILE AT ALL
 public class SpellCheckerRunner {
     public static void main(String[] args) {
-        SpellChecker checker = new SpellChecker();
-        checker.start();
+      SpellChecker checker = new SpellChecker();
+      checker.start();
     }
-}
+  }
